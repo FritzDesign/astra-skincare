28c9fb69478f52e683d4a4fe05c5428a854c37ed
CJ <cjfritz9@gmail.com>
2023-02-03 09:58:57 -0700

    hero text animate in and out states

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,13 @@ import { Stack, Text, Button, Image, Box } from '@chakra-ui/react';
 import { useNavigate } from 'react-router';
 import { HeroProps } from '../models/Props';
 import FMC_Component from '../animations/defaults';
+import { useInView } from 'framer-motion';
 
 const Hero: React.FC<HeroProps> = ({ bgColor, bgImage, heading }) => {
+  const heroText = React.useRef(null);
+  const isInView = useInView(heroText, {
+    margin: '-75% 0% 0% 0%'
+  });
   const navigate = useNavigate();
 
   return (
@@ -37,9 +42,14 @@ const Hero: React.FC<HeroProps> = ({ bgColor, bgImage, heading }) => {
         pos='absolute'
       >
         <FMC_Component
-          animate={{
-            transform: ['translateX(-400px)', 'translateX(0px)']
-          }}
+          animate={
+            isInView
+              ? {
+                  transform: ['translateX(-400px)', 'translateX(0px)']
+                }
+              : { transform: ['translateX(0px)', 'translateX(-400px)'] }
+          }
+          ref={heroText}
           //@ts-ignore
           transition={{ delay: 0.25, duration: 0.75 }}
           fontFamily='Inter'
@@ -57,9 +67,14 @@ const Hero: React.FC<HeroProps> = ({ bgColor, bgImage, heading }) => {
           NEW ASTRA SKIN CARE DROP
         </FMC_Component>
         <FMC_Component
-          animate={{
-            transform: ['translateX(-800px)', 'translateX(0px)']
-          }}
+          animate={
+            isInView
+              ? {
+                  transform: ['translateX(-800px)', 'translateX(0px)']
+                }
+              : { transform: ['translateX(0px)', 'translateX(-800px)'] }
+          }
+          ref={heroText}
           //@ts-ignore
           transition={{ delay: 1, duration: 1 }}
           fontFamily='Marcellus'
@@ -78,9 +93,14 @@ const Hero: React.FC<HeroProps> = ({ bgColor, bgImage, heading }) => {
           _hover={{ filter: 'brightness(1.1)' }}
           cursor='pointer'
           initial={{ opacity: 0 }}
-          animate={{
-            opacity: 1
-          }}
+          animate={
+            isInView
+              ? {
+                  opacity: 1
+                }
+              : { opacity: 0 }
+          }
+          ref={heroText}
           //@ts-ignore
           transition={{ delay: 2, duration: 1 }}
           _active={{ transform: 'scale(0.98)' }}
