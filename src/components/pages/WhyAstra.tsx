4e0988ee907942631879fb6cd0f4f9eeb2b323a0
CJ <cjfritz9@gmail.com>
2023-02-20 15:54:24 -0700

    closes #34, fixed resizing issues with picture

--- src/components/pages/WhyAstra.tsx
+++ src/components/pages/WhyAstra.tsx
@@ -32,7 +32,7 @@ const WhyAstra: React.FC = () => {
           justify='center'
           align='center'
           spacing='1.5rem'
-          mt='88px'
+          mt='64px'
           maxW='1004px'
         >
           <Heading fontFamily='Inter' color='brand.Lavender'>
@@ -80,83 +80,76 @@ const WhyAstra: React.FC = () => {
           </Text>
         </Stack>
       </Flex>
-      <Stack
-        paddingX={['2rem', '3rem', '166px']}
-        direction={['column', 'row']}
-        justify='center'
-        align='center'
-        spacing='102px'
-        my='88px'
-      >
+
+      <Flex w='100%' justify='center'>
         <Stack
-          pos='relative'
-          borderColor='UI.1'
-          borderStartWidth='2px'
-          borderEndWidth='2px'
-          borderTopWidth='2px'
-          borderBottomWidth='2px'
-          width={['280px', '322px']}
-          height={['280px', '322px']}
-          maxWidth='100%'
+          mx={['2rem', '3rem', '3rem', '3rem', '4rem', '166px']}
+          direction={['column', 'column', 'row']}
+          justify='center'
+          align={['flex-start', 'flex-start', 'center']}
+          spacing={['32px', '32px', '56px', '102px', '102px']}
+          my='88px'
+          maxW='1004px'
         >
-          <Image src={bethSmith} />
-          <Icon
-            as={IoMdQuote}
-            pos='absolute'
-            left='-20%'
-            color='brand.Lavender'
-            transform='rotate(180deg)'
-            fontSize='96px'
-          />
-        </Stack>
-        <Stack
-          justify='flex-start'
-          align='flex-start'
-          spacing='32px'
-          width='612px'
-          maxWidth='100%'
-          px={['2rem', '3rem', '0rem']}
-        >
-          <Stack justify='flex-start' align='flex-start' alignSelf='stretch'>
-            <Text
-              fontFamily='Inter'
-              lineHeight='1.2'
-              fontWeight='bold'
-              fontSize='16px'
-              letterSpacing='0.1em'
-              textTransform='uppercase'
+          <Stack
+            pos='relative'
+            minW={['240px', '300px', '240px', '240px', '320px']}
+            minH={['240px', '300px', '240px', '240px', '320px']}
+          >
+            <Image
+              width={['240px', '300px', '240px', '240px', '320px']}
+              height={['240px', '300px', '240px', '240px', '320px']}
+              objectFit='cover'
+              src={bethSmith}
+            />
+            <Icon
+              as={IoMdQuote}
+              pos='absolute'
+              left={['-10%', '-10%', '-20%']}
               color='brand.Lavender'
-              alignSelf='stretch'
+              transform='rotate(180deg)'
+              fontSize='96px'
+            />
+          </Stack>
+          <Stack justify='flex-start' align='flex-start' spacing='32px'>
+            <Stack justify='flex-start' align='flex-start'>
+              <Text
+                fontFamily='Inter'
+                lineHeight='1.2'
+                fontWeight='bold'
+                fontSize='16px'
+                letterSpacing='0.1em'
+                textTransform='uppercase'
+                color='brand.Lavender'
+              >
+                FROM THE OWNER
+              </Text>
+            </Stack>
+            <Text
+              fontFamily='Marcellus'
+              lineHeight='1.2'
+              fontWeight='regular'
+              fontSize={['20px', '20px', '24px']}
+              color='UI.1'
+              opacity='0.8'
             >
-              FROM THE OWNER
+              Loving your skin is an act of self-love, whatever that looks like
+              for you and how good you feel about it. It is personal and unique
+              to each person.”
+            </Text>
+            <Text
+              fontFamily='Poppins'
+              lineHeight='1.5'
+              fontWeight='regular'
+              fontSize='16px'
+              color='UI.1'
+              opacity='0.8'
+            >
+              Beth Smith, Founder and Liscensed Skincare Therapist
             </Text>
           </Stack>
-          <Text
-            fontFamily='Marcellus'
-            lineHeight='1.2'
-            fontWeight='regular'
-            fontSize='24px'
-            color='UI.1'
-            opacity='0.8'
-            alignSelf='stretch'
-          >
-            Loving your skin is an act of self-love, whatever that looks like
-            for you and how good you feel about it. It is personal and unique to
-            each person.”
-          </Text>
-          <Text
-            fontFamily='Poppins'
-            lineHeight='1.5'
-            fontWeight='regular'
-            fontSize='16px'
-            color='UI.1'
-            opacity='0.8'
-            alignSelf='stretch'
-          >
-            Beth Smith, Founder and Liscensed Skincare Therapist
-          </Text>
         </Stack>
-      </Stack>
+      </Flex>
     </Box>
   );
 };
